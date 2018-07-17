904d551d4d25678d775415d1a5bf52474a96cfe8
Emily Jirles <emily.jirles@gmail.com>
2018-07-17 16:21:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function loadIssue() {
     state: "closed",
     number: 5,
     created_at: "2016-03-31 16:23:13 UTC",
+    url: '#',
     body: "Instructions say GET /team and POST /newteam. Rspec wants GET/newteam and POST/team."
   }
 
