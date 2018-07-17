c6ca714cef4ae32a1326e5d8dae8891acd8c1f3c
Emily Jirles <emily.jirles@gmail.com>
2018-07-17 17:01:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-function loadIssue() {
+function loadIssues() {
   const template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
-  for (let i = 0; i < issues.length; i++) {
-    let result = template(issues[i]);
-    document.getElementsByTagName("main")[0].innerHTML += result;
-  }
+  let result = template(issues);
+  document.getElementsByTagName("main")[0].innerHTML += result;
 };
