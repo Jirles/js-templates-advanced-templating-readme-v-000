399df95c49d8c39016e9303e069c496654e46429
Emily Jirles <emily.jirles@gmail.com>
2018-07-17 16:59:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 function loadIssue() {
-  const issue = {
-    state: "closed",
-    number: 5,
-    created_at: "2016-03-31 16:23:13 UTC",
-    url: '#',
-    body: "Instructions say GET /team and POST /newteam. Rspec wants GET/newteam and POST/team."
-  }
-
   const template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
-  const result = template(issue);
-  document.getElementsByTagName("main")[0].innerHTML += result;
-}
+  for (let i = 0; i < issues.length; i++) {
+    const result = template(issues[i]);
+    document.getElementsByTagName("main")[0].innerHTML += result;
+  }
+};
